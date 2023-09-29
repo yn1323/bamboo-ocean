88d5cb24446acb3f712b8dccb91438f8df64b3dc
y-natani <yn1323@gmail.com>
2023-09-30 02:17:26 +0900

    fix: chromatic

---
+++ web/config/main.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+// NOTE: For @storybook/test-runner
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@chakra-ui/storybook-addon', '@storybook/addon-interactions'],
+}
