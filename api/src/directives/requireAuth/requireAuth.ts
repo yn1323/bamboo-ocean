d8f7a8e248a0317470d2a3f7d6a31c585042b284
y-natani <yn1323@gmail.com>
2023-10-08 23:20:48 +0900

    fix

--- api/src/directives/requireAuth/requireAuth.ts
+++ api/src/directives/requireAuth/requireAuth.ts
@@ -18,13 +18,18 @@ type RequireAuthValidate = ValidatorDirectiveFunc<{ roles?: string[] }>
 const validate: RequireAuthValidate = ({ directiveArgs, context }) => {
   const isLocal = process.env.IS_LOCAL === 'true'
 
-  // if (
-  //   !isLocal &&
-  //   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  //   (context as any).event.headers.x_api_key !== process.env.X_API_KEY
-  // ) {
-  //   throw new Error('Unauthorized')
-  // }
+  console.log('hehrehrehrhehre')
+  console.log(isLocal)
+  console.log((context.event as any).headers.x_api_key)
+  console.log(process.env.X_API_KEY)
+
+  if (
+    !isLocal &&
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    (context.event as any).headers.x_api_key !== process.env.X_API_KEY
+  ) {
+    throw new Error('Unauthorized')
+  }
 
   const { roles } = directiveArgs
 
